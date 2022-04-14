bac73d54fe126ac23368d8f14261710695bfcdf8
Sumit Gupta <gsumit232@gmai.com>
2022-04-14 23:42:55 +0530

    new page opened with url

--- raw.js
+++ raw.js
@@ -4,8 +4,27 @@ let browserPromise=puppeteer.launch({headless: false})
 
 
 browserPromise.then(
-    function(browserInsance)
+    function(browser)
     {
         console.log("browser opened");
+        let pagepromimse=browser.newPage();
+        return pagepromimse;
+    }
+
+).then(
+
+    function(page)
+    {
+        console.log("page created");
+        let urlpromise=page.goto("https://github.com/");
+        return urlpromise;
+    }
+).then(
+    function(url)
+    {
+        console.log("github opened");
     }
 )
+
+
+
